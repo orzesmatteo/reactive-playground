0ed5e18f363ebe1e8c2bf0ba720716365bed57a9
Matteo Orzes <orzesmatteo1993@gmail.com>
2024-08-03 17:11:57 +0200

    Migration (#1)
    * Upgrade Gradle, Spring Boot, and Java toolchain versions.
    
    Updated Gradle distribution to version 8.8 and added new properties for network timeout and URL validation. Migrated Spring Boot to version 3.3.2 and updated the Java toolchain to use language version 21. Added a test runtime dependency for JUnit platform launcher.
    
    * Add README and Javadoc comments to pub-sub classes
    
    Introduced a new README.md to outline the project, its goal, and the technologies used. Additionally, added Javadoc comments to `PubSubImpl`, `MessagePubSub`, and `PubSubApi` classes to provide detailed documentation on the methods and their functionality within the publish-subscribe implementation.
    
    * Update build.gradle version

--- src/main/java/it/orz/reactiveplayground/pubsub/PubSubApi.java
+++ src/main/java/it/orz/reactiveplayground/pubsub/PubSubApi.java
@@ -2,10 +2,29 @@ package it.orz.reactiveplayground.pubsub;
 
 import reactor.core.publisher.Flux;
 
+/**
+ * This interface represents a publish-subscribe (pub-sub) API, allowing clients to subscribe and publish messages
+ * to specific topics.
+ */
 public interface PubSubApi {
 
+    /**
+     * Subscribes to a specific topic to receive messages of a specified type.
+     *
+     * @param topic         The topic to subscribe to.
+     * @param messageClass  The class of the messages to subscribe to.
+     * @param <T>           The type of the messages.
+     * @return A Flux stream of messages of the specified type.
+     */
     <T> Flux<T> subscribe(String topic, Class<T> messageClass);
 
+    /**
+     * Publishes a message to the specified topic.
+     *
+     * @param <T> The type of the message
+     * @param topic The topic to which the message will be published
+     * @param message The message to be published
+     */
     <T> void publish(String topic, T message);
 
 }
